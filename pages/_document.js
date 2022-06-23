87c7c6866f329a6d24628041ce992d5a07e31dae
Madison <madisonkanna@gmail.com>
2022-06-22 18:20:29 -0700

    fix(_document): add actual font

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link
-            href="https://fonts.googleapis.com/css2?family=Dosis:wght@300;400&family=Festive&family=Outfit:wght@300;400;500&family=Permanent+Marker&family=Red+Hat+Display:wght@500;700;900&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Dosis:wght@300;400&family=Festive&family=Libre+Baskerville:wght@400;700&family=Outfit:wght@300;400;500&family=Permanent+Marker&family=Red+Hat+Display:wght@500;700;900&display=swap"
             rel="stylesheet"
           />
         </Head>
