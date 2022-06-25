f9603d18515bec9c54ecdac00759176dfca247e3
Madison <madisonkanna@gmail.com>
2022-06-25 10:09:07 -0700

    feat: add listing pages

---
+++ pages/listing/[id].tsx
@@ -0,0 +1,43 @@
+import { FC } from "react";
+import { GetStaticProps } from "next";
+import getData from "../../api";
+import { Listing } from "../../models/Listing";
+
+interface ListingPageProps {
+  listing: Listing;
+}
+
+const ListingPage: FC<ListingPageProps> = ({ listing }) => {
+  console.log(listing, "listing!");
+  // const listing = listings.find((listing) => listing.id === params.listing_id);
+
+  return <div>{listing.name}</div>;
+};
+
+export async function getStaticPaths() {
+  const listings = getData();
+  const paths = listings.map((listing) => ({
+    params: { id: listing.id.toString() },
+  }));
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const { params } = context;
+  const listings = getData();
+  const listing = listings.find(
+    (listing) => listing.id.toString() === params.id
+  );
+  console.log(listing, "listing in props");
+
+  return {
+    props: {
+      listing: listing,
+    },
+  };
+};
+
+export default ListingPage;
